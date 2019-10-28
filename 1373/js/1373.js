dd50f560f0c13543642ac51441ee1be11723681d
tinh980613 <tinh980613>
2019-10-28 11:37:01 +0700

    update

--- 1373/js/1373.js
+++ 1373/js/1373.js
@@ -1,14 +1,51 @@
 var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        rotate:50,
-        loop: true,
-        pagination: {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    rotate: 50,
+    loop: true,
+    pagination: {
         el: '.swiper-pagination',
         clickable: true,
-        },
-        navigation: {
+    },
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 100,
         },
-        });
+
+        1100: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        950: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        850: {
+            slidesPerView: 2,
+            spaceBetween: 45,
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 45,
+        },
+        700: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+    }
+});
